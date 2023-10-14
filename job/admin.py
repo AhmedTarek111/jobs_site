5116b2410a14092e7960a70f33d3548f04acf769
AhmedTarek2000 <ahmedtarekalsaudi@gmail.com>
2023-10-14 15:21:31 +0300

    add inline admin for required skills and education experince

--- job/admin.py
+++ job/admin.py
@@ -4,18 +4,14 @@ from .models import JobEducation_Experience,Jobs,JobRequiredSkills
 class InlineEducation(admin.TabularInline):
     model =JobEducation_Experience
 
-class JobEducationdAdmin(admin.ModelAdmin):
-    inlines =[InlineEducation,]
-
-
 class InlineJobRequiredSkills(admin.TabularInline):
     model =JobRequiredSkills
 
-class JobRequiredSkillsAdmin(admin.ModelAdmin):
-    inlines =[InlineJobRequiredSkills,]
+class JobAdmin(admin.ModelAdmin):
+    inlines =[InlineEducation,InlineJobRequiredSkills]
 
 
-admin.site.register(Jobs,JobRequiredSkillsAdmin)
+admin.site.register(Jobs,JobAdmin)
 admin.site.register(JobEducation_Experience)
 admin.site.register(JobRequiredSkills)  
 
